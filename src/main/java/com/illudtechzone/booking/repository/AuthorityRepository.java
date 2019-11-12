0d8bc1b9668c7140a1522e1a3b69fcabbc663c0f
soorajpn <sooraj.p.n@lxisoft.com>
2019-11-12 12:55:28 +0530

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/illudtechzone/booking/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.illudtechzone.booking.repository;
+
+import com.illudtechzone.booking.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/illudtechzone/booking/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.illudtechzone.booking.web.rest.vm;
